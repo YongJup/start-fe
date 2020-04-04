864218127751f3aed2ef9dc6db9f50b914f92059
Yongjae <k33721@gmail.com>
2020-04-04 18:37:24 +0900

    02 수업 과제

---
+++ 02/app.js
@@ -0,0 +1,84 @@
+// console.log("app.js");
+
+
+// // Create
+// var todos = ['운동'];
+// var todo = '게임';
+// todos.push(todo);
+// console.log(todos);
+
+// // Read
+// todos.forEach(function(todo){
+//     console.log(todo);
+// });
+
+// //Update
+// var todos = ['운동', '게임'];
+// var updateTodo = '게임';
+// var updateIndex = todos.findIndex(function(todo){
+//     return todo === updateTodo;
+// });
+// console.log(updateIndex);
+// todos[updateIndex] = '공부';
+// console.log(todos);
+
+// //Delete
+// var todos = ['운동', '공부', '목욕'];
+// var deleteTodo = '공부';
+// var deleteIndex = todos.findIndex(function(todo) {
+//   return todo === deleteTodo;
+// });
+// todos.splice(deleteIndex, 1);
+// console.log(todos);
+
+// var todos = ['운동', '공부'];
+// var deleteTodo = '공부';
+// var newTodos = todos.filter(function(todo) {
+//   return todo !== deleteTodo;
+// });
+// console.log(newTodos);
+// console.log(todos);
+
+
+// window.alert('1');
+
+// alert('안녕하세요');
+// var who = prompt('당신은 누구세요?');
+// console.log(who);
+
+// isDelete = confirm("정말 삭제하시겠습니까?");
+// console.log(isDelete);
+
+// function sum(a, b){
+//     return a + b;
+// }
+// console.log(sum(1,2));
+
+// var car = {
+//     year: '2014',
+//     starting: function(){
+//         console.log('starting..');
+//     },
+// };
+
+// car.starting();
+
+// (function() {
+//     var a = 'a' ;
+//     console.log(a);   
+// })();
+
+// function sum(x, y) {
+//     if (!y) y = 1;
+//     console.log(x + y);
+// };
+// sum(3);
+// sum(3, 3);
+
+// var text = 'abc def ghi jkl abc def'; // 대상 문자열
+// var testReg = /abc/; // 찾을 문자열
+// var result = testReg.test(text);
+
+// console.log(result); // true
+
+

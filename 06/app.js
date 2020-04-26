663d68300f828f42820fa3aa9ae0fa76bf9c51b2
Yongjae <k33721@gmail.com>
2020-04-26 21:05:04 +0900

    06수업

---
+++ 06/app.js
@@ -0,0 +1,36 @@
+// let a = "aa";
+// const b = "bb";
+// var c = "cc";
+// c = "bb";
+// b = "a";
+// console.log(a, b, c);
+
+// var str = "aaaaa\
+// asdfasfd";
+// const s = `aaaa
+// ddd`;
+
+// console.log(str, s);
+
+// function sum(x, y = 1) {
+//   return x + y;
+// }
+
+// console.log(sum(3));
+
+// const arr = [1, 2, 3];
+
+// const newArr = arr.map((item) => item + 1);
+
+// console.log(newArr);
+
+// document
+//   .getElementById("box")
+//   .addEventListener("click", (event) => console.log(1));
+
+// function sum(x, y, z) {
+//   return x + y + z;
+// }
+// const numbers = [1, 2, 3];
+// console.log(sum(...numbers));
+// console.log(sum(1, 2, 3));

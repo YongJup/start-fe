5bfcd9ec0ffd85d6d1cec220ecf0dff35f1e27ac
Yongjae <k33721@gmail.com>
2020-05-24 13:06:08 +0900

    09 더보기 버튼 추가

--- 09/kakao.js
+++ 09/kakao.js
@@ -1,23 +1,37 @@
 const $btn = document.querySelector('#search-btn');
 const $text = document.querySelector('#search-text');
 const $result = document.querySelector('#result');
+const $moreBtn = document.querySelector('#more');
 const url = 'https://dapi.kakao.com/v2/search/web';
 const headers = { Authorization: 'KakaoAK 9ecc9a02f9fc014a70cd43b8001371be' };
+let page = 1;
 
 function success(data) {
-  const { documents } = data;
+  const { documents, meta } = data;
   const li = documents.map((doc) => {
     return `<li class="list-group-item"><a href="${doc.url}" target="_blank">${doc.contents}</a></li>`;
   });
 
-  $result.innerHTML = `<ul class="list-group list-group-flush">${li.join(
+  $result.innerHTML += `<ul class="list-group list-group-flush">${li.join(
     '',
   )}</ul>`;
+
+  if (!meta.is_end) $moreBtn.hidden = false;
 }
 
 function error() {
+  // eslint-disable-next-line no-alert
   alert('데이터를 가져올 수 없습니다.');
+  $moreBtn.hidden = true;
 }
+
+function fetchSearch(value) {
+  fetch(`${url}?query=${value}&page=${page}`, { headers })
+    .then((res) => res.json())
+    .then(success)
+    .catch(error);
+}
+
 function search() {
   const { value } = $text;
   if (value === '') {
@@ -25,12 +39,19 @@ function search() {
     alert('검색어를 입력하세요');
     return false;
   }
-  fetch(`${url}?query=${value}`, { headers })
-    .then((res) => res.json())
-    .then(success)
-    .catch(error);
-
+  fetchSearch(value);
   return true;
 }
 
-$btn.addEventListener('click', search);
+$btn.addEventListener('click', () => {
+  page = 1;
+  $result.innerHTML = '';
+  search();
+});
+
+$moreBtn.addEventListener('click', () => {
+  page += 1;
+  search();
+});
+
+$moreBtn.hidden = true;
